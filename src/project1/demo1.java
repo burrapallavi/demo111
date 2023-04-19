7236db7e6cac735a2950618bd7e5dd0234a5ee2b
pallavib <burrapallavi16@gmail.com>
2023-04-19 14:08:57 +0530

    third commit:

--- src/project1/demo1.java
+++ src/project1/demo1.java
@@ -8,7 +8,14 @@ public class demo1 {
 		int b=5;
 		int c=a+b;
 		System.out.println(c);
-
+		
+		//added code 
+        int e=6;
+        int f=8;
+        int g=e+f;
+        System.out.println(g);
 	}
+	
+	
 
 }
